12f39d01e3a0a65bf7fb7ae25d0ed14cd8177c46
nicolasauler <nicolasauler@usp.br>
2021-05-11 12:01:16 -0300

    Decrypt working for known keys

--- xor.c
+++ xor.c
@@ -4,31 +4,33 @@
 #define MAXCHAR 1024
 
 void printInt(unsigned int hex[MAXCHAR], int length);
-void getXor(char hex1[MAXCHAR], char hex2[MAXCHAR], int length1, int length2, unsigned int hexXor[MAXCHAR]);
+void getXor0(char hex1[MAXCHAR], char hex2[MAXCHAR], int length1, int length2, unsigned int hexXor[MAXCHAR]);
+void getXor1(char hex3[MAXCHAR], char hex4[MAXCHAR], int length3, int length4, unsigned int hexXor[MAXCHAR]);
 bool menu();
 bool menu2();
 bool getHexChar (char hex[MAXCHAR], int *cont);
 int conversion(char a);
 void hexToInt(char hex[MAXCHAR], char hexRes[MAXCHAR], int length);
 int getModule(int i, int j);
-void printChar(char string[MAXCHAR], int length);
+void printChar(unsigned int string[MAXCHAR], int length);
 
 void hexToInt(char hex[MAXCHAR], char hexRes[MAXCHAR], int length)
 {
 	int cont;
-	printf("traducao abaixo:\n");
-	for(cont = 0; cont < (length); cont += 2)
+	/* printf("traducao abaixo:\n"); */
+	for(cont = 0; cont < length; cont += 2)
 	{
 		hexRes[(cont/2)] = ((conversion(hex[cont])*16) + conversion(hex[cont+1]));
-		printf("%c", hexRes[(cont/2)]);
+		/* printf("%c", hexRes[(cont/2)]); */
 	}
 	/* printChar(hexRes, length); */
 }
 
-void printChar(char string[MAXCHAR], int length)
+void printChar(unsigned int string[MAXCHAR], int length)
 {
 	int cont;
-	for(cont = 0; cont < length; cont++)
+	printf("Your phrase is: \n");
+	for(cont = 0; cont < length/2; cont++)
 	{
 		printf("%c", string[cont]);
 	}
@@ -136,7 +138,7 @@ bool getHexChar (char hex[MAXCHAR], int *cont)
 	return(1);
 }
 
-void getXor(char hex1[MAXCHAR], char hex2[MAXCHAR], int length1, int length2, unsigned int hexXor[MAXCHAR])
+void getXor0(char hex1[MAXCHAR], char hex2[MAXCHAR], int length1, int length2, unsigned int hexXor[MAXCHAR])
 {	
 	int cont, mod;
 	unsigned int hexOne, hexTwo;
@@ -168,10 +170,40 @@ void getXor(char hex1[MAXCHAR], char hex2[MAXCHAR], int length1, int length2, un
 	printf("\n\n");
 }
 
+void getXor1(char hex3[MAXCHAR], char hex4[MAXCHAR], int length3, int length4, unsigned int hexXor[MAXCHAR])
+{	
+	int cont, mod;
+	unsigned int hexOne, hexTwo;
+	mod = getModule(length3, length4);
+	if(length3 > length4)
+	{
+		for(cont = 0; cont < mod; cont++)
+		{
+			hex4[cont+mod] = hex4[cont];
+			hex4[cont] = '0';
+		}
+	}
+	else if(length4 > length3) 
+	{
+		for(cont = 0; cont < mod; cont++)
+		{
+			hex3[cont+mod] = hex3[cont];
+			hex3[cont] = '0';
+		}
+	}
+	printf("Your values are:\n1\t%s\n2\t%s\n\n", hex3, hex4);
+	for(cont = 0; cont < (length3/2); cont++)
+	{
+		hexOne = hex3[cont];
+		hexTwo = hex4[cont];
+		hexXor[cont] = hexOne^hexTwo;
+		printf("%x", hexXor[cont]);
+	}
+	printf("\n\n");	
+}
+
 int main()
 {
-	int cont;
-	unsigned int hexOne, hexTwo;
 	int i, j;
 	char hex1[MAXCHAR], hex2[MAXCHAR], hex3[MAXCHAR], hex4[MAXCHAR];
 	unsigned int hexXor[MAXCHAR];
@@ -189,7 +221,7 @@ int main()
 			return(-1);
 		}
 		printf("Your values were:\n1\t%s\n2\t%s\n", hex1, hex2);
-		getXor(hex1, hex2, i, j, hexXor);
+		getXor0(hex1, hex2, i, j, hexXor);
 	}
 	else
 	{
@@ -209,19 +241,14 @@ int main()
 			printf("Your key was: \n%s", hex2);
 			hexToInt(hex1, hex3, i);
 			hexToInt(hex2, hex4, j);
-			for(cont = 0; cont < i/2; cont++)
-			{
-				hexOne = hex3[cont];
-				hexTwo = hex4[cont];
-				hexXor[cont] = hexOne^hexTwo;
-				printf("%x", hexXor[cont]);
-			}
+			getXor1(hex3, hex4, i, j, hexXor);
+			printChar(hexXor, i);
+		}
 			/* getXor(hex3, hex4, i, j, hexXor); */
 			/*
 			getXor(hex1, hex2, i, j, hexXor);
 			printInt(hexXor, i);
 			*/
-		}
 	}
 	return(0);
 }
