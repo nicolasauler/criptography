bb3ccf460bff25c3066dd88ad9b905b10c2a2e1e
nicolasauler <nicolasauler@usp.br>
2021-05-17 16:40:20 -0300

    added prototype for ordering averages

--- cryptanalysis.h
+++ cryptanalysis.h
@@ -6,4 +6,8 @@ double average (double a, int b, int n);
 
 double lettersPerWord (unsigned int hex[MAXCHAR]);
 
+/* nic, aq vamo comecar usando array e qsort ja implementadas
+ * pra acelerar o processo
+ * depois a gnt pode colocar uma arvore de busca binaria pra deixar
+ * o processo eficiente com listas encadeadas */
 void ordenaEstatistica ();
